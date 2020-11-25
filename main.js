b7965eb5b4c1da980c61d6535d327a971527a53c
Anh dz <nmnanh1235@gmail.com>
2020-11-25 21:31:31 +0700

    fix bugs

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-function changeImage() {
-    document.getElementById('example').src="hitcheems2.png";
-    var audio = new Audio('sound.mp3');
-    audio.play();
-    setTimeout(return_2, 100)
-}
-  function return_2() {
-    element=document.getElementById('example');
-    element.src="hitcheems.png";
-}
+$(document).ready(function() {
+    $("img").click(function() {
+        var audio = new Audio("sound.mp3");
+        audio.play();
+        $(this).attr("src", "hitcheems2.png");
+        setTimeout(() => {$(this).attr("src", "hitcheems.png")} , 100);
+   })
+})
