650f94cbfa97226196f8557bb28c5a862375f6ed
Anh dz <nmnanh1235@gmail.com>
2020-11-25 21:39:19 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ $(document).ready(function() {
         var audio = new Audio("sound.mp3");
         audio.play();
         $(this).attr("src", "hitcheems2.png");
-        //setTimeout(() => {$(this).attr("src", "hitcheems.png")} , 100);
+        setTimeout(() => {$(this).attr("src", "hitcheems.png")} , 100);
    })
 })
