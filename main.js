79374c1e0869b14f834858b3fdd511b58980d83c
Mink Alex Vina <64893437+anh-dz@users.noreply.github.com>
2020-11-26 13:43:12 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $("img").click(function() {
         var audio = new Audio("sound.mp3");
         audio.play();
-        $(this).attr("src", "hitcheems2.png");
+        $(this).attr("src", "https://anh-dz.github.io/hitcheems/hitcheems2.png");
         setTimeout(() => {$(this).attr("src", "hitcheems.png")} , 100);
    })
 })
